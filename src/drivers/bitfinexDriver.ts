f24620f67ab3bb5e50cb6a9fd8bcaf28640fb8ed
Patryk Komar <kontakt.patryk.komar@gmail.com>
2018-08-21 14:48:39 +0200

    Bitfinex driver changes

--- src/drivers/bitfinexDriver.ts
+++ src/drivers/bitfinexDriver.ts
@@ -5,8 +5,27 @@ import { BITFINEX } from "../config/exchanges";
 
 export default class BitfinexDriver extends Driver {
 
+    private marketRandom: number;
+
     prepareUrl(): string {
-        return `https://api.bitfinex.com/v1/pubticker/${this.pair.replace("/", "")}`;
+        const [
+            firstCurrency,
+            secondCurrency
+        ] = this.pair.split("/");
+
+        if (this.pair === "EOS/BTC" || this.pair === "EOS/ETH") {
+            return `https://api.bitfinex.com/v1/pubticker/${firstCurrency}${secondCurrency}`;
+        }
+
+        this.marketRandom = Math.floor(Math.random() * 3);
+
+        if (this.marketRandom === 0) {
+            return `https://api.bitfinex.com/v1/pubticker/${firstCurrency}${secondCurrency}`;
+        } else if (this.marketRandom === 1) {
+            return `http://pagepro.civ.pl/ticker.php?market=bitfinex&coin=${firstCurrency}${secondCurrency}`;
+        } else {
+            return `http://pagepro.civ.pl/ticker.php?market=bitfinex&coin=${firstCurrency}${secondCurrency}`;
+        }
     }
 
     transformData(data: any): any {
