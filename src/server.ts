0d0d823c37d3aee5f70cf05ac191c02952fac320
Tomasz Chmiel <tomasz.chmiel@pagepro.co>
2018-09-03 21:06:57 +0200

    -

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,6 @@ if (process.env.ENV !== "production") {
 /**
  * Start Express server.
  */
-console.log(app.get("port"));
 const server = app.listen(app.get("port"), () => {
   console.log(
     "  App is running at http://localhost:%d in %s mode",
